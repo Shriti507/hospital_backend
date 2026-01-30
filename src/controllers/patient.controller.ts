9b0a4cf8142b589053384dd46deb813905bf3564
Shriti Negi <shritinegi24@gmail.com>
2026-01-31 05:19:04 +0530

    feat: add controller to fetch patient details

--- src/controllers/patient.controller.ts
+++ src/controllers/patient.controller.ts
@@ -1,2 +1,21 @@
-import Express  from "express";
+import express  from "express";
 import { Request,Response } from "express";
+import { PatientService } from "../services/patient.service";
+
+
+export default class PatientController{
+    patientService=new PatientService();
+    app=express();
+
+    getPatientDetails=async(req:Request,res:Response)=>{
+        try{
+            const id = req.params.id as string; // Type assertion to ensure id is a string
+            const detail = await this.patientService.getDetails(id);
+            res.status(200).json(detail);
+        }
+        catch(err){
+            res.status(500).json({ message: "Error fetching patient" });
+        }
+
+    }
+}
